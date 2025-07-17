c77bcba588282effc920c5fba7f2bc21f84dc0ff
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 10:29:47 +0530

    finally Success arbz&farhan

---
+++ src/arbzfarhan/ImranArbz.java
@@ -0,0 +1,14 @@
+package arbzfarhan;
+
+public class ImranArbz
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("welcome arbz");
+		System.out.println("welcome farhan");
+	
+
+	}
+
+}
