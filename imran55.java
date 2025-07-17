439a90927c63a3b0185048758288b5fda48f365f
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 14:27:07 +0530

    added one more file  in central how we can see in eclipse

---
+++ imran55.java
@@ -0,0 +1,8 @@
+public class Imran55
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("Hello imran5 how are you");
+      System.out.println("I am adding some data in central to reflect in Eclipse");
+    }
+  }
