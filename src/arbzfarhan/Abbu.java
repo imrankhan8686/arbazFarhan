2ee68ac0a4b4a7cd8afe20ded6d114c563f361b2
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 10:44:44 +0530

    FOr my abbu

---
+++ src/arbzfarhan/Abbu.java
@@ -0,0 +1,14 @@
+package arbzfarhan;
+
+public class Abbu
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("thank you so much abbu");
+		System.out.println("ur great abbu");
+		System.out.println("love you abbu from arbz&farhan");
+
+	}
+
+}
