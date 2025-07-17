30172c5a194c8507c6451e2ca6e4b41168ac4f94
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 11:03:24 +0530

    from my side success

--- src/arbzfarhan/Abbu.java
+++ src/arbzfarhan/Abbu.java
@@ -8,6 +8,8 @@ public class Abbu
 		System.out.println("thank you so much abbu");
 		System.out.println("ur great abbu");
 		System.out.println("love you abbu from arbz&farhan");
+		System.out.println("mere abbu");
+		System.out.println("update finnally");
 
 	}
 
---
+++ src/arbzfarhan/Childs.java
@@ -0,0 +1,14 @@
+package arbzfarhan;
+
+public class Childs
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("thank you so much my childres");
+		System.out.println("thanks alot ");
+
+
+	}
+
+}
